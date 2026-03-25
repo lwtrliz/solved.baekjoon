236d689b9b5862b8fe5e9bf867dc3f7992229635
lwtrliz <lwtr.liz@gmail.com>
2026-03-25 20:17:12 +0900

    [Bronze II] Title: 소수 찾기, Time: 36 ms, Memory: 34536 KB -BaekjoonHub

--- Python/백준/Bronze/1978. 소수 찾기/소수 찾기.py
+++ Python/백준/Bronze/1978. 소수 찾기/소수 찾기.py
@@ -1,21 +1,22 @@
+import math
+
 n = int(input())
 
-
-
-check_prine = list(map(int, input().split()))
-
-count_prime = 0
+number = list(map(int,input().split()))
+countPrime = 0
 
 for i in range(n):
     notPrime = 0
-    if check_prine[i] <= 1:
+    if number[i] <= 1:
         pass
     else:
-        for j in range(2, check_prine[i]-1):
-            if check_prine[i] % j == 0:
+        sqrt_n = int(math.sqrt(number[i]))
+
+        for j in range(2, sqrt_n+1):
+            if number[i] % j == 0:
                 notPrime += 1
                 break
-        if notPrime == 0:
-            count_prime += 1
+        if notPrime ==0:
+            countPrime += 1
 
-print(count_prime)
+print(countPrime)
