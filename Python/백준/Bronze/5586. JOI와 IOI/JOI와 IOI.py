728032ed7bc5b3691f8a16d3ca73103ca646ab00
lwtrliz <lwtr.liz@gmail.com>
2026-04-04 01:35:10 +0900

    [Bronze II] Title: JOI와 IOI, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Bronze/5586. JOI와 IOI/JOI와 IOI.py
@@ -0,0 +1,11 @@
+string = str(input())
+
+joi = 0
+ioi = 0
+
+for i in range(len(string)-2):
+    if string[i:i+3:] == "JOI":
+        joi += 1
+    elif string[i:i+3:] == "IOI":
+        ioi += 1
+print(f"{joi}\n{ioi}")
