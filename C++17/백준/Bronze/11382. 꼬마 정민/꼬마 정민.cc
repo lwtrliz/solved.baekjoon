0bc9e5457ba1d28e1c4e7045f5cc71e31c5344f9
lwtrliz <lwtr.liz@gmail.com>
2026-04-05 22:07:46 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- C++17/백준/Bronze/11382. 꼬마 정민/꼬마 정민.cc
+++ C++17/백준/Bronze/11382. 꼬마 정민/꼬마 정민.cc
@@ -4,6 +4,10 @@ using namespace std;
 
 int main()
 {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
     long long a, b, c; 
     // 10^12 * 3 일 경우 int 자료형의 범위를 초과
     // int 보다 더 큰 범위인 long long으로 변수 선언
